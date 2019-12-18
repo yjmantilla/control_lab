94db2fe0f810b1ca94b7471a38341784a3a2df9a
Yorguin Jose Mantilla <yjmantilla@gmail.com>
2019-12-18 13:35:34 -0500

    final version

--- p2/smooth_step_response.m
+++ p2/smooth_step_response.m
@@ -4,7 +4,7 @@
 % Basically each second has its own sampling frequency
 clear all
 % Loading of the data
-path = 'Y:\y\sync\current\courses\control\lab\p2\';
+path = 'C:\Users\user\Desktop\code\control_lab\p2\';
 filename = 'to_matlab.csv';
 data = csvread([path filename]);
 
@@ -14,42 +14,27 @@ pv_all = data(:,2)'; % process value, in this case pressure
 % set value is column 3, not needed since there is no control being done
 vop_all = data(:,4)'; % valve opening percentage
 
-
-start = find(vop_all>=max(vop_all), 1, 'first');
+% find when the step stimuli starts
+start = find(vop_all>=max(vop_all), 1, 'first'); 
 time = time_all(start:end);
 time = time - time_all(start); % remove offset to start from zero
 pv = pv_all(start:end);
 vop = vop_all(start:end);
 
-
-% Normalization for easier visualization
-pv_norm = pv/norm(pv);
-vop_norm = vop/norm(vop);
-
 % Smoothing
-span = 0.1 * length(time); % span of 5% of the total number of data points
-pv_smooth = smooth(time,pv,span,'moving'); % smoothing
-pv_smooth_norm = pv_smooth/norm(pv_smooth);
+span = 0.1 * length(time); % span of 10% of the total number of data points
+pv_smooth = smooth(time,pv,span,'moving')'; % smoothing
+
 
 % Plotting
 figure(1)
-plot(time,vop_norm,'r-','LineWidth',2)
-hold on
-plot(time,pv_norm,'g-','LineWidth',3)
-hold on 
-plot(time,pv_smooth_norm,'b-')
-
-legend('Estimulo de apertura de valvula','Respuesta de presión cruda','Respuesta de presión suavizada','Location','SE')
-xlabel('Tiempo (s)')
-ylabel('Valores normalizados para comparación')
-
-figure(2)
 plot(time,pv,'g-','LineWidth',3)
 hold on 
 plot(time,pv_smooth,'b-')
-
+title('Respuesta Experimental vs Suavizada')
 legend('Respuesta de presión cruda','Respuesta de presión suavizada','Location','SE')
 xlabel('Tiempo (s)')
-ylabel('Presión')
+ylabel('Presión ante estimulo de tipo escalón en t=0 (psig)')
 
-save('M12_Grupo3_Presion_datos_depurados.mat','pv_smooth')
+save([path 'M12_Grupo3_Presion_datos_depurados.mat'])
+csvwrite([path 'M12_Grupo3_Presion_datos_depurados.csv'],[time' vop' pv' pv_smooth'])
